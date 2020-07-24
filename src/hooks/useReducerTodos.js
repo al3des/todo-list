9e93947a4b50755eea50bff6cea82ac6dc09c5e7
al3des <alejandrodesalvo@gmail.com>
2020-07-24 17:56:09 +0200

    added hooks
    added hooks for filtering todos and replacing useState() function with useReducer() function

---
+++ src/hooks/useReducerTodos.js
@@ -0,0 +1,24 @@
+import { useReducer } from "react";
+import { v4 as uuid } from "uuid";
+
+export default function useReducerTodos() {
+  let [todos, dispatch] = useReducer((todos, action) => {
+    switch (action.type) {
+      case "ADD":
+        return todos.concat({
+          task: action.task,
+          id: uuid(),
+          isCompleted: false
+        });
+      case "COMPLETE":
+        return todos.map(todo => {
+          return todo.id === action.id ? { ...todo, isCompleted: true } : todo;
+        });
+      case "DELETE":
+        return todos.filter(todo => todo.id !== action.id);
+      default:
+        return todos;
+    }
+  }, []);
+  return [todos, dispatch];
+}
